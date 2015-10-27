69342349aadda7e31af9a96598e349b524f37b4c
zsmithssf <zsmith@softstackfactory.org>
2015-10-27 23:50:12 +0000

    gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
-/**
-* Test task, run test once and exit
-*/
 gulp.task('test', function(done) {
   new Server({
     configFile: __dirname + '/tests/my.conf.js',
